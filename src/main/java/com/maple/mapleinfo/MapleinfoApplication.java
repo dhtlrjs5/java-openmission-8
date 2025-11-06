f070eeddb98b4226ac0b1139ea7c14ef78e77181
Sigeon Oh <dhtlrjs5@naver.com>
2025-11-06 11:55:25 +0900

    feat: 프로젝트 초기 설정

---
+++ src/main/java/com/maple/mapleinfo/MapleinfoApplication.java
@@ -0,0 +1,13 @@
+package com.maple.mapleinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapleinfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapleinfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maple/mapleinfo/MapleinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maple.mapleinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapleinfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
