5e6a4a4f1ac9df1df4423c9ec236117041c5ee42
Sigeon Oh <dhtlrjs5@naver.com>
2025-11-07 14:20:39 +0900

    feat: 장기간 미접속 사용자의 입력이 주어진 경우 예외 처리 로직 구현

--- src/main/java/com/maple/mapleinfo/client/MapleApiClient.java
+++ src/main/java/com/maple/mapleinfo/client/MapleApiClient.java
@@ -1,15 +1,19 @@
 package com.maple.mapleinfo.client;
 
 import com.maple.mapleinfo.dto.CharacterBasicInfoDto;
+import com.maple.mapleinfo.exception.CharacterNotFoundException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
+@Slf4j
 @Component
 public class MapleApiClient {
 
@@ -40,14 +44,18 @@ public class MapleApiClient {
 
         HttpEntity<Void> entity = new HttpEntity<>(createHeaders());
 
-        ResponseEntity<String> response = restTemplate.exchange(
-                url,
-                HttpMethod.GET,
-                entity,
-                String.class
-        );
+        try {
+            ResponseEntity<String> response = restTemplate.exchange(
+                    url,
+                    HttpMethod.GET,
+                    entity,
+                    String.class
+            );
 
-        return response.getBody();
+            return response.getBody();
+        } catch (HttpClientErrorException e) {
+            throw new CharacterNotFoundException("잘못된 캐릭터명 입니다.");
+        }
     }
 
     public CharacterBasicInfoDto getCharacterBasicInfo(String ocid) {
@@ -59,13 +67,17 @@ public class MapleApiClient {
 
         HttpEntity<Void> entity = new HttpEntity<>(createHeaders());
 
-        ResponseEntity<CharacterBasicInfoDto> response = restTemplate.exchange(
-                url,
-                HttpMethod.GET,
-                entity,
-                CharacterBasicInfoDto.class
-        );
+        try {
+            ResponseEntity<CharacterBasicInfoDto> response = restTemplate.exchange(
+                    url,
+                    HttpMethod.GET,
+                    entity,
+                    CharacterBasicInfoDto.class
+            );
 
-        return response.getBody();
+            return response.getBody();
+        } catch (HttpClientErrorException e) {
+            throw new CharacterNotFoundException("캐릭터 정보를 불러올 수 없습니다.");
+        }
     }
 }
