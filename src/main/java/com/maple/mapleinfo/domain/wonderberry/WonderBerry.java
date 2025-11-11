69e940bcf34127bfc6be624a682561a87efec3af
Sigeon Oh <dhtlrjs5@naver.com>
2025-11-11 13:36:28 +0900

    feat: 펫 뽑기 도메인 구현

--- src/main/java/com/maple/mapleinfo/domain/wonderberry/WonderBerry.java
+++ src/main/java/com/maple/mapleinfo/domain/wonderberry/WonderBerry.java
@@ -1,13 +1,51 @@
 package com.maple.mapleinfo.domain.wonderberry;
 
 import java.util.List;
+import java.util.Random;
 
 public class WonderBerry {
 
     private final List<Item> items;
+    private final WonderStatistics statistics;
+    private final Random random = new Random();
 
     public WonderBerry(List<Item> items) {
         this.items = items;
+        statistics = new WonderStatistics();
     }
 
+    public WonderResult useWonderBerry() {
+
+        double randomValue = random.nextDouble() * 100;
+        double cumulative = 0L;
+
+        for (Item item : items) {
+            cumulative += item.getProbability();
+
+            if (randomValue <= cumulative) {
+                statistics.addCount();
+                statistics.addCost(5500L);
+                statistics.addItem(item.getName());
+
+                return new WonderResult(item, statistics);
+            }
+        }
+
+        throw new IllegalStateException("확률 합계가 100%가 아닙니다.");
+    }
+
+    public WonderResult useTenTimes() {
+
+        for (int i = 0; i < 10; i++) {
+            useWonderBerry();
+        }
+
+        return new WonderResult(null, statistics);
+    }
+
+    public WonderResult reset() {
+        statistics.reset();
+
+        return new WonderResult(null, statistics);
+    }
 }
---
+++ src/main/java/com/maple/mapleinfo/domain/wonderberry/WonderResult.java
@@ -0,0 +1,10 @@
+package com.maple.mapleinfo.domain.wonderberry;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class WonderResult {
+
+    private final Item item;
+    private final WonderStatistics statistics;
+}
