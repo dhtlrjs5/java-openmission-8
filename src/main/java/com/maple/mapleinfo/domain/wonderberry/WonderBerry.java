cefd14cbe88ca0c752acec0b2a1dade7220bda00
Sigeon Oh <dhtlrjs5@naver.com>
2025-11-11 13:29:20 +0900

    feat: 펫 뽑기 통계 도메인 구현

---
+++ src/main/java/com/maple/mapleinfo/domain/wonderberry/WonderBerry.java
@@ -0,0 +1,13 @@
+package com.maple.mapleinfo.domain.wonderberry;
+
+import java.util.List;
+
+public class WonderBerry {
+
+    private final List<Item> items;
+
+    public WonderBerry(List<Item> items) {
+        this.items = items;
+    }
+
+}
---
+++ src/main/java/com/maple/mapleinfo/domain/wonderberry/WonderStatistics.java
@@ -0,0 +1,29 @@
+package com.maple.mapleinfo.domain.wonderberry;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WonderStatistics {
+
+    private Long count = 0L;
+    private Long cost = 0L;
+    private final Map<String, Long> itemCount = new HashMap<>();
+
+    public void addCount() {
+        count++;
+    }
+
+    public void addCost(long amount) {
+        cost += amount;
+    }
+
+    public void addItem(String itemName) {
+        itemCount.put(itemName, itemCount.getOrDefault(itemName, 0L) + 1);
+    }
+
+    public void reset() {
+        count = 0L;
+        cost = 0L;
+        itemCount.clear();
+    }
+}
