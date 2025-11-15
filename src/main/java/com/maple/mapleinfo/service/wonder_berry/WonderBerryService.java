d5252895aa6ea6b6284beb8366eeb8b6d939c356
Sigeon Oh <dhtlrjs5@naver.com>
2025-11-15 18:58:08 +0900

    refactor: WonderBerryService 메서드 분리 및 함수 라인 감소

--- src/main/java/com/maple/mapleinfo/service/wonder_berry/WonderBerryService.java
+++ src/main/java/com/maple/mapleinfo/service/wonder_berry/WonderBerryService.java
@@ -22,44 +22,55 @@ public class WonderBerryService {
     private static final String FILE_NAME = "wonder_berry.json";
     private static final String RESOURCE_PATH = "/data/" + FILE_NAME;
 
-    private static final String ERROR_FILE_NOT_FOUND = FILE_NAME + SUFFIX_NOT_FOUND;
-    private static final String ERROR_FILE_FAIL_LOADING = FILE_NAME + SUFFIX_FAIL_LOADING;
-
     private final WonderBerry wonderBerry;
 
     public WonderBerryService() {
 
         ObjectMapper mapper = new ObjectMapper();
 
-        JsonNode root;
-        try (InputStream inputStream = getClass().getResourceAsStream(RESOURCE_PATH)) {
-            if (inputStream == null) {
-                throw new FileNotFoundException(ERROR_FILE_NOT_FOUND);
-            }
-            root = mapper.readTree(inputStream);
-        } catch (Exception e) {
-            throw new IllegalStateException(ERROR_FILE_FAIL_LOADING);
-        }
+        JsonNode root = loadJsonRoot(mapper);
 
+        List<Item> items = createItems(root);
+
+        this.wonderBerry = new WonderBerry(items);
+    }
+
+    private List<Item> createItems(JsonNode root) {
         List<Item> items = new ArrayList<>();
 
         for (JsonNode node : root.path("RARE")) {
-            items.add(new Item(
-                    node.get("name").asText(),
-                    node.get("probability").asDouble(),
-                    Rarity.RARE
-            ));
+            Item item = createItem(node, Rarity.RARE);
+            items.add(item);
         }
 
         for (JsonNode node : root.path("NORMAL")) {
-            items.add(new Item(
-                    node.get("name").asText(),
-                    node.get("probability").asDouble(),
-                    Rarity.NORMAL
-            ));
+            Item item = createItem(node, Rarity.NORMAL);
+            items.add(item);
+        }
+        return items;
+    }
+
+    private Item createItem(JsonNode node, Rarity rarity) {
+        return new Item(
+                node.get("name").asText(),
+                node.get("probability").asDouble(),
+                rarity
+        );
+    }
+
+    private JsonNode loadJsonRoot(ObjectMapper mapper) {
+        JsonNode root;
+
+        try (InputStream inputStream = getClass().getResourceAsStream(RESOURCE_PATH)) {
+            if (inputStream == null) {
+                throw new FileNotFoundException(FILE_NAME + SUFFIX_NOT_FOUND);
+            }
+            root = mapper.readTree(inputStream);
+        } catch (Exception e) {
+            throw new IllegalStateException(FILE_NAME + SUFFIX_FAIL_LOADING);
         }
 
-        this.wonderBerry = new WonderBerry(items);
+        return root;
     }
 
     public WonderResult useBerry() {
