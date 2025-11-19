d259ea106e9a921d591995c72976f4d1125b838c
Sigeon Oh <dhtlrjs5@naver.com>
2025-11-20 08:45:09 +0900

    test: 원더베리 테스트의 값이 잘못 입력되어있는  버그 수정

--- src/test/java/com/maple/mapleinfo/domain/wonder_berry/WonderBerryTest.java
+++ src/test/java/com/maple/mapleinfo/domain/wonder_berry/WonderBerryTest.java
@@ -58,19 +58,19 @@ class WonderBerryTest {
         // then
         assertEquals("RARE", result.getItem().getName(), "RARE가 뽑혀야 한다");
         assertEquals(1L, stats.getCount(), "사용 횟수가 1 증가해야 한다");
-        assertEquals(5500L, stats.getCost(), "비용이 5500L 증가해야 한다"); // 상수 사용이 불가능하여 매직넘버 유지
+        assertEquals(5400L, stats.getCost(), "비용이 5400L 증가해야 한다"); // 상수 사용이 불가능하여 매직넘버 유지
         assertEquals(1L, stats.getItemCount().get("RARE"), "RARE 획득 횟수가 1 증가해야 한다");
     }
 
     @Test
-    @DisplayName("성공: 10회 사용 시 10번의 사용이 누적되며 비용이 55000L 증가한다")
+    @DisplayName("성공: 10회 사용 시 10번의 사용이 누적되며 비용이 54000L 증가한다")
     void useTenTimes_Success() {
         // given
         // 10회 동안 항상 NORMAL을 뽑도록 Mock
         when(mockRandom.nextDouble()).thenReturn(0.50);
 
-        // 예상되는 총 비용: 5500L * 10
-        final long EXPECTED_TOTAL_COST = 55000L;
+        // 예상되는 총 비용: 5400L * 10
+        final long EXPECTED_TOTAL_COST = 54000L;
         final long EXPECTED_COUNT = 10L;
 
         // when
@@ -79,7 +79,7 @@ class WonderBerryTest {
 
         // then
         assertEquals(EXPECTED_COUNT, stats.getCount(), "사용 횟수가 10 증가해야 한다");
-        assertEquals(EXPECTED_TOTAL_COST, stats.getCost(), "비용이 5500L * 10 증가해야 한다");
+        assertEquals(EXPECTED_TOTAL_COST, stats.getCost(), "비용이 5400L * 10 증가해야 한다");
         assertEquals(EXPECTED_COUNT, stats.getItemCount().get("NORMAL"), "아이템 획득 횟수가 10 증가해야 한다");
     }
 
